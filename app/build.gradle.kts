451bfd85ccf2ab8a76c962b75a489b5f08eb051d
graciellanath <graciellanathania8@gmail.com>
2025-11-18 11:04:56 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,13 @@ android {
 }
 
 dependencies {
-
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
