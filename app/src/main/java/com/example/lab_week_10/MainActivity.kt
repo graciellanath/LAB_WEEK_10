3a071b1bc4542eb7bf7ad0d1a47a46d1b08ad548
graciellanath <graciellanathania8@gmail.com>
2025-11-18 11:16:12 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -5,17 +5,28 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.lab_week_10.database.Total
+import com.example.lab_week_10.database.TotalDatabase
 import com.example.lab_week_10.viewmodels.TotalViewModel
 
-
 class MainActivity : AppCompatActivity() {
-    private val viewModel by lazy {
-        ViewModelProvider(this)[TotalViewModel::class.java]
-    }
+
+    private lateinit var db: TotalDatabase
+    private lateinit var viewModel: TotalViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Buat database dan viewmodel
+        db = prepareDatabase()
+        viewModel = ViewModelProvider(this)[TotalViewModel::class.java]
+
+        // Inisialisasi nilai awal dari database
+        initializeValueFromDatabase()
+
+        // Siapkan ViewModel dan observer
         prepareViewModel()
     }
 
@@ -24,16 +35,39 @@ class MainActivity : AppCompatActivity() {
             getString(R.string.text_total, total)
     }
 
-    private fun prepareViewModel(){
-        // Observe the LiveData object
-        viewModel.total.observe(this, {
+    private fun prepareViewModel() {
+        // Observe LiveData total
+        viewModel.total.observe(this) { updateText(it) }
 
-            updateText(it)
-        })
+        // Tombol tambah total
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
     }
 
+    private fun prepareDatabase(): TotalDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java,
+            "total-database"
+        ).allowMainThreadQueries().build()
+    }
 
+    private fun initializeValueFromDatabase() {
+        val totalList = db.totalDao().getTotal(ID)
+        if (totalList.isEmpty()) {
+            db.totalDao().insert(Total(id = 1, total = 0))
+        } else {
+            viewModel.setTotal(totalList.first().total)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update(Total(ID, viewModel.total.value ?: 0))
+    }
+
+    companion object {
+        const val ID: Long = 1
+    }
 }
